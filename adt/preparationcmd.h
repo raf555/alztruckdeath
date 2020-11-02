4c63744aae63e2fbcae61e96afe96744b3e3d975
raf555 <irsoekarno888@gmail.com>
2020-11-02 07:15:16 +0700

    revisi adt preparationcmd

--- adt/preparationcmd.h
+++ adt/preparationcmd.h
@@ -17,12 +17,13 @@ typedef struct {
 	JAM waktu; // banyaknya waktu yang dibutuhkan per-cmd
 	Kata perintah; // command
 	Kata target; // target cmd (bisa wahana / bahan)
-	targetcmd targetvalue; // target
+	targetcmd targetvalue; // value yg akan diubah utk target
 } cmd;
 
 /* selektor */
 #define WaktuCMD(c) (c).waktu
 #define PerintahCMD(c) (c).perintah
+#define TargetCMD(c) (c).target
 #define TargetBuild(c) (c).targetvalue.titik
 #define TargetUpgrade(c) (c).targetvalue.wahana
 #define TargetBuy(c) (c).targetvalue.jumlah
